86e0bec058d37c1e70713ac4323589a00d27ff23
Ankur Kumar <ankmuz007+github@gmail.com>
2024-10-14 16:22:21 +0530

    SCDR_SFDC-01: tailwind CSS project

---
+++ TailwindCSS-React-Project2/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './Card.jsx'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind test!</h1>
+      <Card />
+      <br />
+      <Card />
+      <br />
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ TailwindCSS-React-Project2/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Card from './Card.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
