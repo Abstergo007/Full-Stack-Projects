2394340be9327a6887c5e5ea847fcae691156d0d
Ankur Kumar <ankmuz007+github@gmail.com>
2024-10-14 13:35:26 +0530

    SCDR_SFDC-01: argument added to onclick functions

--- React-Project-1/src/App.jsx
+++ React-Project-1/src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from "react"
 
 function App() {
   const [counter, setCounter] = useState(15);
-  const addValue = () => {
-    setCounter(counter+1)
+  const addValue = (delta) => {
+    setCounter(counter+delta)
   }
-  const subValue = () => {
-    setCounter(counter-1)
+  const subValue = (delta) => {
+    setCounter(counter-delta)
   }
   
   return (
     <>
       <h1>Chai n React</h1>
       <h2>Counter value: {counter}</h2>
-      <button onClick={addValue}>Add value</button>
-      <button onClick={subValue}>Remove Value</button>
+      <button onClick={() => addValue(5)}>Increment by 5</button>
+      <button onClick={() => addValue(10)}>Increment by 10</button>
+      <button onClick={() => subValue(-5)}>Decrement by 5</button>
     </>
   )
 }
