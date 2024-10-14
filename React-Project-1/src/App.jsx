5474694856a5f0c58a604c0ac96b9b300c3d02be
Ankur Kumar <ankmuz007+github@gmail.com>
2024-10-14 13:07:41 +0530

    SCDR_SFDC-01: Code to add a counter button in page.

--- React-Project-1/src/App.jsx
+++ React-Project-1/src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 
 function App() {
-
+  const [counter, setCounter] = useState(15);
+  const addValue = () => {
+    setCounter(counter+1)
+  }
+  const subValue = () => {
+    setCounter(counter-1)
+  }
+  
   return (
-    <h1>This is the first page</h1>
+    <>
+      <h1>Chai n React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={subValue}>Remove Value</button>
+    </>
   )
 }
 
